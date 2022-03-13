056452d2a4c79c17e30525e859eca54225062702
Arianna Avalos <68700321+ariavalosm@users.noreply.github.com>
2022-03-13 00:38:36 -0500

    Add files via upload

---
+++ Clase 4/Desafio complementario/Funciones/javascript/app.js
@@ -0,0 +1,13 @@
+var iva = prompt ("Introduzca un monto");
+
+var importe = parseInt(iva);
+
+var iva = importe * 0.18;
+
+var total = importe + iva ;
+
+document.write ('<h1> importe: ' + importe + ' PEN  </h1>')
+document.write ('<h1> iva: ' + iva+ ' PEN </h1>')
+document.write ('<h1> total: ' + total+ ' PEN  </h1>')
+
+

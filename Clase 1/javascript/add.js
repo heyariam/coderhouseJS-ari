797aa24f461d032b1cbb37e16724d8518522a087
Arianna Avalos <68700321+ariavalosm@users.noreply.github.com>
2022-03-13 00:36:52 -0500

    Add files via upload

---
+++ Clase 1/javascript/add.js
@@ -0,0 +1,9 @@
+
+
+let nombreUsuario= prompt("¿cual es tu nombre?");
+let edad= prompt("Ingresa tu anio nacimiento");
+let anio= 2021
+let edadUsuario= anio - edad
+let pais= prompt("Ingresar pais");
+
+alert("Hola" + " " + nombreUsuario + " " + edadUsuario + " " + pais);
